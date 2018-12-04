2bcd60010eda05e632a3a31298472001a4d9e60f
Nathan Adduci <nathanadduci@gmail.com>
2018-12-04 17:56:20 -0500

    Various changes to server.py

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, send_file, request, redirect, url_for, send_from_directory
+from flask import Flask, render_template, send_file, request, redirect, url_for, send_from_directory, Response
 from database import userPassExists, userExists, insertUser4, insertUser6, getMainPage
 #from werkzeug.utils import secure_filename
 #from PyPDF2 import PdfFileReader, PdfFileWriter
@@ -14,7 +14,6 @@ app = Flask(__name__)
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
 
 def allowed_file(filename):
 	return '.' in filename and \
@@ -40,12 +39,18 @@ def index():
 	<p>%s</p>
 	% "<br>".join(os.listdir(app.config['UPLOAD_FOLDER'],))#"""
 
+
+
+uploadForm = '<form id="uploadcontent" method="post" style="display: inline-block" width="20%" height="25%" enctype="multipart/form-data"> <input type="file" id="pdfbox" style="border: 1px solid black" accept="application/pdf"><br> <input type="submit" id="upload" value="Upload"><br> </form>'
+
 @app.route('/login', methods=['POST'])
 def loginU():
 	print('login post')
-	print(userPassExists(request.form.get('username'), request.form.get('password')))
+	if userPassExists(request.form.get('username'), request.form.get('password')):
+		print('auth checked out')
+		return 'True'
 	print('login pass')
-	return ''
+	return 'False'#send_file('templates/data/upload.txt')
 	
 @app.route('/create-account', methods=['POST'])
 def accountMaker():
@@ -55,7 +60,7 @@ def accountMaker():
 		insertUser4(request.form.get('username'), request.form.get('password'), request.form.get('firstname'), request.form.get('lastname'))
 		print('user Inserted')
 	print('create pass')
-	return ''
+	return Response(uploadForm, mimetype='text/html')
 
 @app.route('/files/<path:path>')
 def serveFile(path):
@@ -72,6 +77,13 @@ def index1():
 @app.route('/create-account/')
 def index2():
 	return render_template('account.html')
+
+@app.route('/style.css')
+def style():
+	return send_file('templates/style.css')
+@app.route('/create-account/style.css')
+def styleCA():
+	return send_file('templates/style.css')
 
 if __name__ == '__main__':
 	app.run(debug=True, host='192.168.1.139', port='5397')
