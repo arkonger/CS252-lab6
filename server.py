c271addd9a47aae26c060d0d42af5e5adcdd427b
Nathan Adduci <nathanadduci@gmail.com>
2018-12-07 10:20:19 -0500

    User options (#1)
    * beginning work on user login options
    
    * beginning work on user login options
    
    * beginning work on user login options
    
    * beginning work on user login options
    
    * beginning work on user login options
    
    * beginning work on user login options
    
    * beginning work on user login options
    
    * beginning work on user login options
    
    * beginning work on user login options
    
    * beginning work on user login options
    
    * beginning work on user login options
    
    * beginning work on user login options
    
    * beginning work on user login options
    
    * beginning work on user login options
    
    * beginning work on user login options
    
    * finishing up testing, ready to implement features
    
    * Created iterative user writing from the database.
    
    * adding some formatting
    
    * f'd up the merge
    
    * PI is behind, commiting
    
    * cleaning up js
    
    * cleaning up js
    
    * Delete .gitignore
    
    * cleaning up js
    
    * Delete forlater.sh
    
    * Delete commit.sh
    
    * end me
    
    * fixed weird git artifacts?
    
    * unremoved important things
    
    * becky like divs?
    
    * made a variable global
    
    * testing ajax
    
    * testing ajax
    
    * testing ajax
    
    * testing ajax
    
    * testing ajax
    
    * testing ajax
    
    * testing ajax
    
    * testing ajax
    
    * testing ajax
    
    * testing ajax
    
    * testing ajax
    
    * testing ajax
    
    * testing ajax
    
    * testing ajax
    
    * testing ajax
    
    * testing ajax
    
    * testing ajax
    
    * testing ajax
    
    * testing ajax
    
    * testing ajax
    
    * testing ajax
    
    * testing ajax
    
    * testing ajax
    
    * testing ajax
    
    * testing ajax
    
    * testing ajax
    
    * testing ajax
    
    * testing ajax
    
    * testing ajax
    
    * testing ajax
    
    * testing ajax
    
    * (potentially) Final commit before presentation

--- server.py
+++ server.py
@@ -1,7 +1,10 @@
 import os
-from flask import Flask, render_template, send_file, request, redirect, url_for, send_from_directory, Response
-from database import userPassExists, userExists, insertUser4, insertUser6, getMainPage
-#from werkzeug.utils import secure_filename
+import sys
+from pathlib import Path
+from flask import Flask, render_template, send_file, request, redirect, url_for, send_from_directory, Response, abort, flash
+from werkzeug.utils import secure_filename
+from database import userPassExists, userExists, insertUser4, insertUser6, getMainPage, getLocale, insertLocale
+from werkzeug.utils import secure_filename
 #from PyPDF2 import PdfFileReader, PdfFileWriter
 
 
@@ -50,9 +53,14 @@ uploadForm = """<form id="uploadcontent" method="post" style="display: inline-bl
 @app.route('/login', methods=['POST'])
 def loginU():
 	print('login post')
+	print(request.args)
+	print(request.data)
+	print(request.form)
+	print(request.files)
+	print(request.values)
 	if userPassExists(request.form.get('username'), request.form.get('password')):
 		print('auth checked out')
-		return 'True'
+		return send_file('templates/data/upload.txt')
 	print('login pass')
 	return 'False'#send_file('templates/data/upload.txt')
 	
@@ -64,14 +72,7 @@ def accountMaker():
 		insertUser4(request.form.get('username'), request.form.get('password'), request.form.get('firstname'), request.form.get('lastname'))
 		print('user Inserted')
 	print('create pass')
-	return send_file('templates/data/upload.txt') #"""<form id="uploadcontent" method="post" style="display: inline-block"
-  	#width="20%" height="25%" enctype="multipart/form-data">
-  	#<input type="file" id="pdfbox" style="border: 1px solid black"
-    #	accept="application/pdf"><br>
-  	#<input type="submit" name="submit" value="Upload"><br>
-#	</form>"""
 
-@app.route('/upload', methods=['POST'])
 def pdfAdder():
 	print('create post')
 	for a1, a2 in request.args:
@@ -91,11 +92,26 @@ def serveFile(path):
 	path = path.replace('%20', ' ', path.count('%20'))
 	#path = path.replace('/', '\\/', path.count('/'))
 	print(path)
-	return send_file('files/'+path, attachment_filename=path)
+	if path is not None:
+		if userExists(path):
+			locale = getLocale(path)
+			if locale is not None:
+				fileT = Path('files/'+locale)
+				if fileT.is_file():
+					return send_file('files/'+locale, attachment_filename=locale)
+		else:
+			fileT = Path('files/'+path)
+			if fileT.is_file():
+				return send_file('files/'+path, attachment_filename=path)
+	return abort(404)
 
 @app.route('/')
 def index1():
-	return render_template('landing.html')
+	f = open('templates/landing.html','r')
+	fileContent = f.read()
+	#print(getMainPage())
+	fileContent = fileContent.replace('$REPLACE_WITH_USER_LIST', getMainPage())
+	return fileContent
 
 @app.route('/create-account/')
 def index2():
@@ -108,7 +124,63 @@ def style():
 def styleCA():
 	return send_file('templates/style.css')
 
-if __name__ == '__main__':
-	app.run(debug=True, host='192.168.1.139', port='5397')
+@app.route('/', methods = ['POST'])
+def upload_file_req2():
+	return upload_file(request)
+		
+@app.route('/upload', methods = ['POST'])
+def upload_file_req():
+	return upload_file(request)
+
+def upload_file(request):
+	if request.method == 'POST':
+		#print(request.args)
+		#print(request.data)
+		print(request.form)
+		print(request.files)
+		print(request.values)
+		print('if1')
+		sys.stdout.flush()
+		if 'file' not in request.files:
+			print('file not found')
+			sys.stdout.flush()
+			#flash('No file part')
+			return 'file not found'#redirect(request.url)
+		file = request.files['file']
+		username = request.form.get('username')
+		sys.stdout.flush()
+		print('if2')
+		if file.filename == '':
+			print('file not given')
+			sys.stdout.flush()
+			#flash('No selected file')
+			return 'file not given'#redirect(request.url)
+		print('if3')
+		if file and allowed_file(file.filename) and username is not None:
+			print('file found and written')
+			sys.stdout.flush()
+			filename = secure_filename(file.filename)
+			file.save(os.path.join('./files/', filename))
+			insertLocale(username, filename)
+			#return redirect('./templates/landing.html')
+			return 'Successful'
+		else:
+			print(file)
+			print(allowed_file(file.filename))
+			print(uesrname is not None)
+		f = open('Hello World.pdf','w')
+		f.write(request.form.get('file'))
+		f.close()
+		print('POSTING')
+		sys.stdout.flush()
+		for a1, a2 in request.args:
+			print(a1 + "\t" + a2)
+			sys.stdout.flush()
+		#f = request.files['file']
+		#f.save(secure_filename(f.filename))
+		sys.stdout.flush()
+		return 'Successful'
 	
 
+if __name__ == '__main__':
+	app.run(debug=True, host='192.168.1.139', port='5397')
